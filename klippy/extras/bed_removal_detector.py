54fc59757787e600564e07d39679c08e0c1dc784
Harry Loud <admin@muon3d.com>
2024-10-09 13:55:34 +0100

    .

--- klippy/extras/bed_removal_detector.py
+++ klippy/extras/bed_removal_detector.py
@@ -8,37 +8,35 @@ class BedRemovalDetector:
         self.bed_heater = self.printer.lookup_object('heater_bed')
         # self.toolhead = self.printer.lookup_object('toolhead')
         self.gcode = self.printer.lookup_object('gcode')
-        self.gcode.respond_info("STARTING")
         self.bed_removed = False
-        #self.printer.register_event_handler("klippy:ready", self.handle_ready)
+        self.printer.register_event_handler("klippy:ready", self.handle_ready)
+        self.gcode.respond_info("INIT COMPLETE")
 
-    # def handle_ready(self):
-    #     reactor = self.printer.get_reactor()
-    #     reactor.register_timer(self.check_bed_temperature, reactor.NOW)
+    def handle_ready(self):
+        reactor = self.printer.get_reactor()
+        reactor.register_timer(self.check_bed_temperature, reactor.NOW)
 
-    # def check_bed_temperature(self, eventtime):
-    #     temp, _ = self.bed_heater.get_temp(eventtime)
-    #     if temp < self.threshold_temp:
-    #         if not self.bed_removed:
-    #             self.handle_bed_removal()
-    #     else:
-    #         if self.bed_removed:
-    #             self.handle_bed_reconnection()
-    #     return eventtime + 1.0  # Check every second
+    def check_bed_temperature(self, eventtime):
+        temp, _ = self.bed_heater.get_temp(eventtime)
+        if temp < self.threshold_temp:
+            if not self.bed_removed:
+                self.handle_bed_removal()
+        else:
+            if self.bed_removed:
+                self.handle_bed_reconnection()
+        return eventtime + 1.0  # Check every second
 
-    # def handle_bed_removal(self):
-    #     self.bed_removed = True
-    #     self.bed_heater.set_temp(0.0)
-    #     logging.info("Bed removed")
-    #     self.gcode.respond_info("Bed removed. Print paused.")
-    #     # self.toolhead.pause()
-    #     # Additional code to notify UI can be added here
+    def handle_bed_removal(self):
+        self.bed_removed = True
+        self.bed_heater.set_temp(0.0)
+        self.gcode.respond_info("Bed removed. Print paused.")
+        # self.toolhead.pause()
+        # Additional code to notify UI can be added here
 
-    # def handle_bed_reconnection(self):
-    #     self.bed_removed = False
-    #     logging.info("Bed removed")
-    #     self.gcode.respond_info("Bed reconnected. You may resume printing.")
-    #     # Additional code to update UI can be added here
+    def handle_bed_reconnection(self):
+        self.bed_removed = False
+        self.gcode.respond_info("Bed reconnected. You may resume printing.")
+        # Additional code to update UI can be added here
 
     # def cmd_START_PRINT(self, gcmd):
     #     if self.bed_removed:
