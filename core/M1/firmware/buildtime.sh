a8f4ba11bde03b7cf98d2b1f3ff57f7264a0adde
Harry Loud <admin@muon3d.com>
2025-04-11 04:25:54 +0100

    moved to sperate build and flash scripts

--- core/M1/firmware/buildtime.sh
+++ core/M1/firmware/buildtime.sh
@@ -12,11 +12,8 @@ MAIN_CONFIG="${SCRIPT_DIR}/config.rp2040"
 TOOLHEAD_CONFIG="${MAIN_CONFIG}"
 RPI_CONFIG="${SCRIPT_DIR}/config.rpi"
 
-OPENOCD_MAIN_CONFIG="${SCRIPT_DIR}/openocd.main.config"
-OPENOCD_TOOLHEAD_CONFIG="${SCRIPT_DIR}/openocd.toolhead.config"
+MAIN_BIN="${SCRIPT_DIR}/rp2040.bin"
 
-# Stop klipper service
-sudo service klipper stop || { echo "Error: Failed to stop klipper service."; exit 1; }
 
 # Change directory to ~/klipper
 cd ${KLIPPER_DIR} || { echo "Error: Failed to change directory to ~/klipper."; exit 1; }
@@ -24,23 +21,26 @@ cd ${KLIPPER_DIR} || { echo "Error: Failed to change directory to ~/klipper."; e
 # Build steps for MAIN_CONFIG
 make clean KCONFIG_CONFIG="${MAIN_CONFIG}" || { echo "Error: 'make clean' for ${MAIN_CONFIG} failed."; exit 1; }
 #make menuconfig KCONFIG_CONFIG="${MAIN_CONFIG}" || { echo "Error: 'make menuconfig' for ${MAIN_CONFIG} failed."; exit 1; }
+
 #Force creation of BIN File
 if ! grep -q "^CONFIG_RPXXXX_HAVE_BOOTLOADER=y$" "${MAIN_CONFIG}"; then
     echo "CONFIG_RPXXXX_HAVE_BOOTLOADER=y" >> "${MAIN_CONFIG}"
 fi
 make KCONFIG_CONFIG="${MAIN_CONFIG}" || { echo "Error: 'make' for ${MAIN_CONFIG} failed."; exit 1; }
+cp "${KLIPPER_OUT_DIR}/klipper.bin" "${MAIN_BIN}"
+echo "Main Binary created and statshed at: ${MAIN_BIN}"
 
-# Program device using the main OpenOCD config
-openocd -f "${OPENOCD_MAIN_CONFIG}" -c "program ${KLIPPER_OUT_DIR}/klipper.bin 0x10000000 verify reset; exit" || { echo "Error: OCD Flasing the MAIN MCU Failed."; exit 1; }
-openocd -f "${OPENOCD_TOOLHEAD_CONFIG}" -c "program ${KLIPPER_OUT_DIR}/klipper.bin 0x10000000 verify reset; exit" || { echo "Error: Flasing the TOOLHEAD MCU Failed"; exit 1; }
+
+
+echo "Building and Flashing RPI Host Microcontroller, does not require seperate reflashing"
+
+cd ~/klipper/
+sudo cp ./scripts/klipper-mcu.service /etc/systemd/system/
+systemctl enable klipper-mcu.service
 
 # Build and flash for RPI_CONFIG
 make clean KCONFIG_CONFIG="${RPI_CONFIG}" || { echo "Error: 'make clean' for ${RPI_CONFIG} failed."; exit 1; }
 #make menuconfig KCONFIG_CONFIG="${RPI_CONFIG}" || { echo "Error: 'make menuconfig' for ${RPI_CONFIG} failed."; exit 1; }
 make flash KCONFIG_CONFIG="${RPI_CONFIG}" || { echo "Error: 'make flash' for ${RPI_CONFIG} failed."; exit 1; }
 
-# Start klipper service
-sudo service klipper start || { echo "Error: Failed to start klipper service."; exit 1; }
-
-
-echo "Firmware Flashed Sucessfully"
+echo "Flashed RPI Host MCU"
