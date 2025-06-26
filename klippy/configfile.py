1498f133eeb15bb01610ae58456770bae39e4674
Harry Loud <admin@muon3d.com>
2025-06-26 15:10:08 +0100

    REMOVED muon3d_fan and probe and replaced them with more reliable macros

--- klippy/configfile.py
+++ klippy/configfile.py
@@ -406,7 +406,16 @@ class ConfigAutoSave:
             raise gcmd.error(msg)
         # Request a restart
         gcode = self.printer.lookup_object('gcode')
-        gcode.request_restart('restart')
+
+        # If requested restart or no restart just flag config saved
+        restartTest = gcmd.get_int("NO_RESTART", None)
+        if restartTest is None or restartTest != 1:
+            # Request a restart
+            gcode.request_restart('restart')
+        else:
+            # flag config updated to false since config saved with no restart
+            self.save_config_pending = False
+            gcode.respond_info("Config File update without restart successful")
 
 
 ######################################################################
